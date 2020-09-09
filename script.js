810840fe85644315567891027a00e16d59e6fd43
Andre Grech <silver.grech@gmail.com>
2020-09-10 00:42:13 +1000

    Opting for an array of objects, containing choices arrays with objects containign answer properties

--- script.js
+++ script.js
@@ -1,20 +1,42 @@
-var questions = [{
-    title: 'Dummy text',
-    choices: ['opt1','opt2','opt3','opt4'],
-    answer: 'opt2'
-}
-{
-    title: 'Dummy text',
-    choices: ['opt1','opt2','opt3','opt4'],
-    answer: 'opt2'   
-}
-{
-    title: 'Dummy text',
-    choices: ['opt1','opt2','opt3','opt4'],
-    answer: 'opt2'
-}
-{
-    title: 'Dummy text',
-    choices: ['opt1','opt2','opt3','opt4'],
-    answer: 'opt2'
-}]
+var questions = [
+  {
+    title: "Dummy text",
+    choices: [
+      { id: 1, diplayText: "1" },
+      { id: 2, diplayText: "2" },
+      { id: 3, diplayText: "3" },
+      { id: 4, diplayText: "4" },
+    ],
+    answerId: 1,
+  },
+  {
+    title: "Dummy text",
+    choices: [
+      { id: 1, diplayText: "1" },
+      { id: 2, diplayText: "2" },
+      { id: 3, diplayText: "3" },
+      { id: 4, diplayText: "4" },
+    ],
+    answerId: 1,
+  },
+  {
+    title: "Dummy text",
+    choices: [
+      { id: 1, diplayText: "1" },
+      { id: 2, diplayText: "2" },
+      { id: 3, diplayText: "3" },
+      { id: 4, diplayText: "4" },
+    ],
+    answerId: 1,
+  },
+  {
+    title: "Dummy text",
+    choices: [
+      { id: 1, diplayText: "1" },
+      { id: 2, diplayText: "2" },
+      { id: 3, diplayText: "3" },
+      { id: 4, diplayText: "4" },
+    ],
+    answerId: 1,
+  },
+];
