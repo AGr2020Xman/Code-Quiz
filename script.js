520bc7b99ff6d4f4fc0c94845f716f7eb2f9ea44
Andre Grech <silver.grech@gmail.com>
2020-09-10 00:39:33 +1000

    trying an array of questions

---
+++ script.js
@@ -0,0 +1,20 @@
+var questions = [{
+    title: 'Dummy text',
+    choices: ['opt1','opt2','opt3','opt4'],
+    answer: 'opt2'
+}
+{
+    title: 'Dummy text',
+    choices: ['opt1','opt2','opt3','opt4'],
+    answer: 'opt2'   
+}
+{
+    title: 'Dummy text',
+    choices: ['opt1','opt2','opt3','opt4'],
+    answer: 'opt2'
+}
+{
+    title: 'Dummy text',
+    choices: ['opt1','opt2','opt3','opt4'],
+    answer: 'opt2'
+}]
